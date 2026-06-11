5eb634063fc032f0e3ba4a9f8dad4c93cecf065e
Luis Henao <lhenao@consensussa.com>
2026-06-11 12:40:11 -0500

    chore(cors): permitir todas las origenes con credentials

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { HttpExceptionFilter } from './http-exception.filter.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: true, credentials: true });
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
